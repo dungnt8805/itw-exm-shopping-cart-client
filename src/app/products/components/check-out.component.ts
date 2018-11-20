8bed15f49f6e247e2a09a39f89a5e4b6a848eee0
spiritwild <dungnt8805@gmail.com>
2018-11-20 18:52:06 +0700

    abc

--- src/app/products/components/check-out.component.ts
+++ src/app/products/components/check-out.component.ts
@@ -12,42 +12,63 @@ import { Cart } from '../../models/cart';
 export class CheckOutComponent implements OnInit {
 	cart: Cart;
 	requesting = false;
+	_sortByTitle = false;
+	_sortByPrice = false;
+	_sortByQty = true;
+	_sortByTotal = false;
 	constructor(private activatedRoute: ActivatedRoute, private productsService: ProductsService) {
 	}
 	ngOnInit() {
 		this.activatedRoute.data.subscribe((data: { cart: Cart }) => {
 			this.cart = data.cart;
+			this.sort('qty');
 		});
 	}
 
-	increaseQty(item) {
-		this.requesting = true;
-		item.quantity++;
-		this.updateCart(() => {});
-	}
-	decreaseQty(item) {
-		item.quantity--;
-		if (item.quantity <= 0) {
-			item.quantity = 0;
+	sort(sortBy) {
+		this.resetSortLabel();
+		switch (sortBy) {
+			case "qty":
+			default:
+				this._sortByQty = true;
+				this.cart.items.sort((a, b) => {
+					return a.quantity - b.quantity;
+				})
+				break;
+			case 'price': 
+				this._sortByPrice = true;
+				this.cart.items.sort((a, b) => {
+					return a.price - b.price;
+				});
+				break;
+			case 'title':
+				this._sortByTitle = true;
+				this.cart.items.sort((a, b) => {
+					const nameA = a.title.toUpperCase();
+					const nameB = b.title.toUpperCase();
+					if (nameA < nameB) {
+					    return -1;
+					}
+					if (nameA > nameB) {
+					    return 1;
+					}
+					return 0;
+				});
+				break;
+			case 'total':
+				this._sortByTotal = true;
+				this.cart.items.sort((a, b) => {
+					return (a.price * a.quantity) - (b.price * b.quantity);
+				});
+				break;
+
 		}
-		this.updateCart(() => {});
-	}
-	removeItem(item, idx) {
-		item.quantity = 0;
-		this.requesting = true;
-		this.updateCart(() => {
-			this.cart.items.splice(idx, 1)
-		});
 	}
 
-	updateCart(callback) {
-		this.requesting = true;
-		this.productsService.updateCart(this.cart.items).subscribe(cartResp => {
-			this.cart.items = cartResp.items;
-			this.cart.price = cartResp.price;
-			this.cart.total = cartResp.total;
-			this.requesting = false;
-			callback(cartResp);
-		});
+	resetSortLabel() {
+		this._sortByQty = false;
+		this._sortByPrice = false;
+		this._sortByTitle = false;
+		this._sortByTotal = false;
 	}
 }
